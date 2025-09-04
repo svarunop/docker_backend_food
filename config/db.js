e8c2085dc1c5fda781261ebebb56845b74cfbdc2
Saivarun <s8saivarun@gmail.com>
2025-09-04 11:20:07 +0530

    db url

--- config/db.js
+++ config/db.js
@@ -1,13 +1,7 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port : process.env.DB_PORT
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect(err => {
   if (err) throw err;
